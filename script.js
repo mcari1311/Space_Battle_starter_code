410d6e04771cea67b20f39f3a66ff5997ad8394f
Marie <mcari1311@gmail.com>
2022-06-25 15:58:40 -0400

    pseudocode

---
+++ script.js
@@ -0,0 +1,8 @@
+//alert tab
+//attack the alien ship 
+//if the ship survives, it attacks you (repeat)
+//if you destroy the ship you have 2 options: attack or retreat
+//if you retreat, it's game over
+//if you chose attack then go onto the next alien ship
+//you win the game when all the aliens are destroyed
+//you lose the game if you are destroyed
