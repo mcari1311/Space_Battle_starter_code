8f83f26413d718c4926f3c967512d878b759558f
Marie <mcari1311@gmail.com>
2022-06-25 17:34:21 -0400

    more psuedocode

--- script.js
+++ script.js
@@ -6,3 +6,38 @@
 //if you chose attack then go onto the next alien ship
 //you win the game when all the aliens are destroyed
 //you lose the game if you are destroyed
+
+//fix uss hello world name 
+
+// class Ships {
+//     constructor(hull, firepower, accuracy) {
+//         this.hull = hull
+//         this.firepower = firepower
+//         this.accuracy = accuracy 
+//     }
+
+//     accuracy() {
+        
+//     }
+//     hull() {
+
+//     }
+//     firepower() {
+
+//     }
+// } 
+
+// const USSHelloWorld = {
+//     hull: 20 ,
+//     firepower: 5 , 
+//     accuracy: .7 ,
+// }
+
+
+// const Alien = {
+//     hull: 
+//     firepower: 
+//     accuracy:
+// }
+
+//maybe best idea is to create a ship class because you'll need multiple ships for the aliens
